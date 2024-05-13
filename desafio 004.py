627a2ae5ef1361d070f5233b3cf896975e9493b8
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-05-13 20:50:30 -0300

    Add files via upload

---
+++ desafio 004.py
@@ -0,0 +1,14 @@
+algo = input('digite algo: ')
+print('o tipo primitivo desse valor é:', type(algo))
+print('esse valor é alfanumérico?', algo.isalnum())
+print('esse valor é alfabético?', algo.isalpha())
+print('esse valor é numérico?', algo.isnumeric())
+print('esse valor é formado apenas por números?', algo.isalnum())
+print('esse valor é composto apenas por letras maiúsculas?', algo.isupper())
+print('esse valor é composto apenas por letras minúsculas?', algo.islower())
+print('esse valor é um dígito?', algo.isdigit())
+print('esse valor é formado apenas por espaços?', algo.isspace())
+print('esse valor é um número decimal?', algo.isdecimal())
+
+
+
