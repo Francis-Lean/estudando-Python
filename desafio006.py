22495b08c633b04a6435c129d04866a94cbeaea6
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-05-13 20:51:09 -0300

    Add files via upload

---
+++ desafio006.py
@@ -0,0 +1,4 @@
+N = int(input('digite um número: '))
+print('o dobro desse valor é {}'.format(N*2))
+print('o triplo desse valor é {}'.format(N*3))
+print('a raiz quadrada desse valor é {:.0f}'.format(N**0.5))
