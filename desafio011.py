d07062a41196a0c3401aa6535470717c7365b1a9
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-05-16 21:25:21 -0300

    Add files via upload

---
+++ desafio011.py
@@ -0,0 +1,4 @@
+largura = float(input('largura da parede: '))
+altura = float(input('altura da parede: '))
+print('Sua parede tem dimensão de {}x{} e sua área é de {}m2.'.format(largura, altura, largura * altura))
+print('para pintar essa parede você precisará de {}l de tinta2.'.format(largura * altura / 2))
