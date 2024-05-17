3f0fab54f98e5f9e01bc554c238f4b87fa3f17dc
Francis Lean <167146805+Francis-Lean@users.noreply.github.com>
2024-05-16 21:25:49 -0300

    Add files via upload

---
+++ desafio 013.py
@@ -0,0 +1,3 @@
+salário = float(input('Qual é o salário do funcionário? R$'))
+print('Um funcionário que ganhava R${:.1f}, com 15% de aumento, passa a receber R${:.1f}'.format(salário, salário * 0.15 + salário))
+
